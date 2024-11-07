2943e5d0d76307a6ed367eeffdc29a9852366862
prasadhonrao <honrao.prasad@gmail.com>
2024-11-07 09:29:23 +0000

    Add react-router-dom for routing and update App.js to use Outlet

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,7 +1,7 @@
+import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <main>
         <Container>
-          <HomePage />
+          <Outlet />
         </Container>
       </main>
       <Footer />
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.css';
 import './assets/styles/index.css';
 import App from './App';
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomePage />} />
+      <Route path="*" element={<div>Not Found</div>} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
