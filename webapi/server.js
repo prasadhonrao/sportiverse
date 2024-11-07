7e77d07404b75c2667df1468ad16efa684160a10
prasadhonrao <honrao.prasad@gmail.com>
2024-11-07 11:28:01 +0000

    Add Express setup to web API with basic server configuration

--- webapi/server.js
+++ webapi/server.js
@@ -1,1 +1,13 @@
-console.log('Server-side code running');
+import express from 'express';
+
+const port = process.env.port || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Sai Sports Web API is running');
+});
+
+app.listen(port, () => {
+  console.log(`Sai Sports Web API is running on port ${port}`);
+});
