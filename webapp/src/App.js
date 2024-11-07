3a4ec686fa87ae680e636586b79563966ec179cb
prasadhonrao <honrao.prasad@gmail.com>
2024-11-07 07:34:24 +0000

    Add Header component and update App.js to include Bootstrap layout

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,16 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
 function App() {
-  return <h1>Sai Sports Shop</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to Sai Sports Shop</h1>
+        </Container>
+      </main>
+    </>
+  );
 }
 
 export default App;
