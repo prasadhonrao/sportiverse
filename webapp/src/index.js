7455259967b4b28232f37b24aa7caec74f5ced92
prasadhonrao <honrao.prasad@gmail.com>
2024-11-07 10:34:19 +0000

    - Add ProductPage component and update routing for product details - Use default CSS

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './assets/styles/bootstrap.css';
 import './assets/styles/index.css';
 import App from './App';
 import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductPage />} />
       <Route path="*" element={<div>Not Found</div>} />
     </Route>
   )
---
+++ webapp/src/pages/ProductPage.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+
+const ProductPage = () => {
+  const { id: productId } = useParams();
+
+  const product = products.find((product) => product._id === productId);
+
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews}reviews`} />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: {product.price}</ListGroup.Item>
+            <ListGroup.Item>{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className="btn btn-block" type="button" disabled={product.countInStock === 0}>
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductPage;
