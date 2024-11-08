5df26d475bdf1a345dc9b8eac29ef52acbda8d55
prasadhonrao <honrao.prasad@gmail.com>
2024-11-08 19:40:58 +0000

    - Add async handler middleware and integrate that with product routes - Return HTTP status code from home route

--- webapi/routes/homeRoutes.js
+++ webapi/routes/homeRoutes.js
@@ -3,11 +3,11 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Sai Sports Web API is running...');
+  res.status(200).send('Sai Sports Web API is running...');
 });
 
 router.get('/version', (req, res) => {
-  res.send('1.0');
+  res.status(200).send('1.0');
 });
 
 export default router;
--- webapi/routes/productRoutes.js
+++ webapi/routes/productRoutes.js
@@ -1,14 +1,29 @@
 import express from 'express';
+import asyncHandler from '../middlewares/asyncHandler.js';
+import Product from '../models/productModel.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json(products);
-});
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  })
+);
 
-router.get('/:id', (req, res) => {
-  const product = products.find((p) => (p._id = req.params.id));
-  res.send(product);
-});
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    console.log(req.params.id);
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).send({ message: 'Product not found' });
+    }
+
+    return res.status(200).send(product);
+  })
+);
 
 export default router;
