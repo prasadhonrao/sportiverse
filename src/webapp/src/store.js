a4582e088b67016d2440d3b320f82eb687883a63
prasadhonrao <honrao.prasad@gmail.com>
2024-11-09 09:50:00 +0000

    Enable Redux DevTools only in development environment

--- src/webapp/src/store.js
+++ src/webapp/src/store.js
@@ -6,7 +6,7 @@ const store = configureStore({
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
-  devTools: true,
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store;
