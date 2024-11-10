8e8e983e9e3b6d721dafd6a3db1f42cf9712ae07
prasadhonrao <honrao.prasad@gmail.com>
2024-11-10 22:47:23 +0000

    Refactor cartSlice to use updateCart utility for price calculations and state updates

--- src/webapp/src/slices/cartSlice.js
+++ src/webapp/src/slices/cartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addDecimals } from '../utils/numberUtils';
+import { updateCart } from '../utils/cartUtils';
 
 export const cartSlice = createSlice({
   name: 'cart',
@@ -13,20 +13,7 @@ export const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-
-      // Calculate items price
-      state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0));
-
-      // Calculate shipping price (free shipping if items price > 100 otherwise 10)
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-
-      // Calculate tax price (15% of items price)
-      state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
-
-      // Calculate total price
-      state.totalPrice = (Number(state.itemsPrice) + Number(state.shippingPrice) + Number(state.taxPrice)).toFixed(2);
-
-      localStorage.setItem('cart', JSON.stringify(state));
+      return updateCart(state);
     },
   },
 });
