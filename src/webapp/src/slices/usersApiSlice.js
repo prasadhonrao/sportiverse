55fddc71c3ea20c7ac8a69549f0181d8a5deb41f
prasadhonrao <honrao.prasad@gmail.com>
2024-11-11 11:36:42 +0000

    Add usersApiSlice to handle user login functionality

---
+++ src/webapp/src/slices/usersApiSlice.js
@@ -0,0 +1,17 @@
+import { USERS_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/login`,
+        method: 'POST',
+        body: data,
+      }),
+      providesTags: ['User'],
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
