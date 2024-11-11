7981e2676769136ed869432980c761339f23d4a6
prasadhonrao <honrao.prasad@gmail.com>
2024-11-11 04:10:40 +0000

    Add user authentication and management related method placeholders to userController

---
+++ src/webapi/controllers/userController.js
@@ -0,0 +1,74 @@
+// @desc    Authenticate user and return JWT token
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Login route' });
+});
+
+// @desc    Register a new user
+// @route   POST /api/users/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Register route' });
+});
+
+// @desc    Log user out / clear cookie
+// @route   POST /api/users/logout
+// @access  Private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Logout route' });
+});
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get user profile route' });
+});
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Update user profile' });
+});
+
+// desc   Get all users
+// route  GET /api/users
+// access Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get all users route' });
+});
+
+// @desc    Get user by ID
+// @route   GET /api/users/:id
+// @access  Private / Admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Get user by ID route' });
+});
+
+// @desc    Update user
+// @route   POST /api/users/:id
+// @access  Private / Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Update user route' });
+});
+
+// desc   Delete a user
+// route  DELETE /api/users/:id
+// access Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Delete user route' });
+});
+
+export {
+  loginUser,
+  logoutUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
