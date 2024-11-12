6140ab8e320072c0a1f02497df65efccc06267a5
prasadhonrao <honrao.prasad@gmail.com>
2024-11-12 11:42:53 +0000

    Implement order creation and retrieval functionality in orderController

--- src/webapi/controllers/orderController.js
+++ src/webapi/controllers/orderController.js
@@ -5,21 +5,50 @@ import Order from '../models/orderModel.js';
 // route  POST /api/orders
 // access Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Add order items' });
+  const { orderItems, shippingAddress, paymentMethod, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+    const createdOrder = await order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 // desc   Get logged in users order
 // route  GET /api/orders/mine
 // access Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get my orders' });
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // desc   Get order by Id
 // route  GET /api/orders/:id
 // access Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get order by id' });
+  const order = await Order.findById(req.params.id).populate('user', 'name email'); // populate user with name and email;
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 // desc   Update order to paid
