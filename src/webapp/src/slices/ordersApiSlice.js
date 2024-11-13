c9ab53a5320c262d30eb3e927948b2c23d4d6232
prasadhonrao <honrao.prasad@gmail.com>
2024-11-13 10:34:33 +0000

    Add PayPal payment mutation and client ID query to orders API slice

--- src/webapp/src/slices/ordersApiSlice.js
+++ src/webapp/src/slices/ordersApiSlice.js
@@ -1,4 +1,5 @@
 import { ORDERS_URL } from '../constants';
+import { PAYPAL_URL } from '../constants';
 import { apiSlice } from './apiSlice';
 
 export const ordersApiSlice = apiSlice.injectEndpoints({
@@ -20,7 +21,23 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
       providesTags: ['Order'],
       keepUnusedDataFor: 5,
     }),
+    payOrder: builder.mutation({
+      query: ({ orderId, details }) => ({
+        url: `${ORDERS_URL}/${orderId}/pay`,
+        method: 'PUT',
+        body: { ...details },
+      }),
+      providesTags: ['Order'],
+    }),
+    getPaypalClientId: builder.query({
+      query: () => ({
+        url: PAYPAL_URL,
+        method: 'GET',
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
-export const { useCreateOrderMutation, useGetOrderDetailsQuery } = ordersApiSlice;
+export const { useCreateOrderMutation, useGetOrderDetailsQuery, usePayOrderMutation, useGetPaypalClientIdQuery } =
+  ordersApiSlice;
