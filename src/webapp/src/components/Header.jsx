055c845d398147f35a6784c5f9d664d65de2b542
prasadhonrao <honrao.prasad@gmail.com>
2024-11-13 15:35:00 +0000

    Add admin dropdown menu to Header for admin user navigation

--- src/webapp/src/components/Header.jsx
+++ src/webapp/src/components/Header.jsx
@@ -51,7 +51,20 @@ const Header = () => {
                   )}
                 </Nav.Link>
               </LinkContainer>
-              {userInfo ? (
+              {userInfo && userInfo.isAdmin ? (
+                <NavDropdown title={userInfo.name} id="username">
+                  <LinkContainer to="/admin/productlist">
+                    <NavDropdown.Item>Products</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/userlist">
+                    <NavDropdown.Item>Users</NavDropdown.Item>
+                  </LinkContainer>
+                  <LinkContainer to="/admin/orderlist">
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              ) : userInfo ? (
                 <NavDropdown title={userInfo.name} id="username">
                   <LinkContainer to="/profile">
                     <NavDropdown.Item>Profile</NavDropdown.Item>
