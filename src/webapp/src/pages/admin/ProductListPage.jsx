440bb6abb78a831e88b4b0b43e67331c7473d705
prasadhonrao <honrao.prasad@gmail.com>
2024-11-13 16:51:03 +0000

    Implement ProductListPage with loading, error handling, and product management features

--- src/webapp/src/pages/admin/ProductListPage.jsx
+++ src/webapp/src/pages/admin/ProductListPage.jsx
@@ -1,7 +1,77 @@
 import React from 'react';
 
+import { Table, Button, Row, Col } from 'react-bootstrap';
+import { FaEdit, FaPlus, FaTrash } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+import { useGetProductsQuery } from '../../slices/productsApiSlice';
+
 const ProductListPage = () => {
-  return <div>ProductListPage</div>;
+  const { data, isLoading, error, refetch } = useGetProductsQuery();
+
+  const createProductHandler = () => {
+    console.log('Create Product');
+  };
+
+  const deleteHandler = (id) => {
+    console.log('Delete Product');
+  };
+
+  return (
+    <>
+      <Row className="align-items-center">
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className="text-end">
+          <Button className="my-3" onClick={createProductHandler}>
+            <FaPlus /> Create Product
+          </Button>
+        </Col>
+      </Row>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error.data.message}</Message>
+      ) : (
+        <>
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>PRICE</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.products?.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>${product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                    <Button as={Link} to={`/admin/product/${product._id}/edit`} variant="light" className="btn-sm mx-2">
+                      <FaEdit />
+                    </Button>
+                    <Button variant="danger" className="btn-sm" onClick={() => deleteHandler(product._id)}>
+                      <FaTrash style={{ color: 'white' }} />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ProductListPage;
