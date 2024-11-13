837548b0941c16ed895228ec7c02715da08fe6af
prasadhonrao <honrao.prasad@gmail.com>
2024-11-13 10:34:19 +0000

    Wrap RouterProvider with PayPalScriptProvider for PayPal integration

--- src/webapp/src/index.js
+++ src/webapp/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -46,7 +47,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />{' '}
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
