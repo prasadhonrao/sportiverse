faf9af814a9af48549a51b3ff6c122205e5be936
prasadhonrao <honrao.prasad@gmail.com>
2024-11-13 12:38:47 +0000

    Update PayPal config endpoint to return client ID as an object. This is important to render PayPal buttons correctly

--- src/webapi/server.js
+++ src/webapi/server.js
@@ -35,7 +35,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
 // PayPal routes
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
 
 // Custom middlewares
 app.use(errorHandler);
