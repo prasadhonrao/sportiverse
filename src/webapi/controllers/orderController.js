e7b35dd46ee5e04971497afee24668bf16e4ae8e
prasadhonrao <honrao.prasad@gmail.com>
2024-11-13 15:37:17 +0000

    Update getOrders route to use GET method and return all orders with user details

--- src/webapi/controllers/orderController.js
+++ src/webapi/controllers/orderController.js
@@ -82,10 +82,11 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 });
 
 // desc   Get all orders
-// route  POST /api/orders
+// route  GET /api/orders
 // access Private/Admin
 const getOrders = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'get orders' });
+  const orders = await Order.find({}).populate('user', 'id name');
+  res.status(200).json(orders);
 });
 
 export { addOrderItems, getMyOrders, getOrderById, updateOrderToPaid, updateOrderToDelivered, getOrders };
