7fd74528efd18af1a48bfefa3b8663fa7bd2ac52
prasadhonrao <honrao.prasad@gmail.com>
2024-11-13 21:20:41 +0000

    Add ProductEditPage component and route for editing products

--- src/webapp/src/index.js
+++ src/webapp/src/index.js
@@ -25,6 +25,7 @@ import ProductListPage from './pages/admin/ProductListPage';
 
 import PrivateRoute from './components/PrivateRoute';
 import AdminRoute from './components/AdminRoute';
+import ProductEditPage from './pages/admin/ProductEditPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -48,6 +49,7 @@ const router = createBrowserRouter(
       <Route path="" element={<AdminRoute />}>
         <Route path="/admin/orderlist" element={<OrderListPage />} />
         <Route path="/admin/productlist" element={<ProductListPage />} />
+        <Route path="/admin/product/:id/edit" element={<ProductEditPage />} />
       </Route>
     </Route>
   )
