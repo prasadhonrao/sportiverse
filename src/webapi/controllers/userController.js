92cb44b45bb938642527f5706982140464b6659b
prasadhonrao <honrao.prasad@gmail.com>
2024-11-14 13:42:08 +0000

    Implement update user functionality in userController

--- src/webapi/controllers/userController.js
+++ src/webapi/controllers/userController.js
@@ -137,10 +137,28 @@ const getUserById = asyncHandler(async (req, res) => {
 });
 
 // @desc    Update user
-// @route   POST /api/users/:id
-// @access  Private / Admin
+// @route   PUT /api/users/:id
+// @access  Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Update user route' });
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 // @desc    Delete user
