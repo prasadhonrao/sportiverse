f38f36de9516efa2b9a7edcdcbd4b33449984baf
prasadhonrao <honrao.prasad@gmail.com>
2024-11-14 13:06:14 +0000

    Add deleteProduct functionality to productController and routes

--- src/webapi/routes/productRoutes.js
+++ src/webapi/routes/productRoutes.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import { protect, admin } from '../middlewares/authMiddleware.js';
-import { getProducts, getProductById, createProduct, updateProduct } from '../controllers/productController.js';
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from '../controllers/productController.js';
 
 const router = express.Router();
 router.route('/').get(getProducts).post(protect, admin, createProduct);
-router.route('/:id').get(getProductById).put(protect, admin, updateProduct);
+router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
 
 export default router;
