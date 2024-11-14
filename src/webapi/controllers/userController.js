6893fdab9be05a5293d119e68175dffbeade9d09
prasadhonrao <honrao.prasad@gmail.com>
2024-11-14 13:41:36 +0000

    Implement get user by ID functionality in userController

--- src/webapi/controllers/userController.js
+++ src/webapi/controllers/userController.js
@@ -124,9 +124,16 @@ const getUsers = asyncHandler(async (req, res) => {
 
 // @desc    Get user by ID
 // @route   GET /api/users/:id
-// @access  Private / Admin
+// @access  Private/Admin
 const getUserById = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get user by ID route' });
+  const user = await User.findById(req.params.id).select('-password');
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 // @desc    Update user
