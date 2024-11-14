a87dd648507afc3927fc7a91e52e70c118dd0713
prasadhonrao <honrao.prasad@gmail.com>
2024-11-14 13:03:32 +0000

    Refactor HomePage to use destructured data from useGetProductsQuery

--- src/webapp/src/pages/HomePage.jsx
+++ src/webapp/src/pages/HomePage.jsx
@@ -4,19 +4,19 @@ import Product from '../components/Product';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomePage = () => {
-  const { data: products, isLoading = false, isError = false } = useGetProductsQuery();
+  const { data, isLoading = false, isError = false } = useGetProductsQuery();
+  console.log(data);
   return (
     <>
       {isLoading && <h2>Loading...</h2>}
       {isError && <h2>Error</h2>}
 
       <Row>
-        {products &&
-          products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
+        {data?.products?.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
       </Row>
     </>
   );
