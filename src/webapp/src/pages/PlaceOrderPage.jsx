37336f5c961635b1c9b661ee4e99fd49b459db48
prasadhonrao <honrao.prasad@gmail.com>
2024-11-15 23:29:05 +0000

    Integrate PayPal payment processing and price calculation in order management

--- src/webapp/src/pages/PlaceOrderPage.jsx
+++ src/webapp/src/pages/PlaceOrderPage.jsx
@@ -1,38 +1,32 @@
 import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
-import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap';
-
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import CheckoutSteps from '../components/CheckoutSteps';
 import Loader from '../components/Loader';
-
 import { useCreateOrderMutation } from '../slices/ordersApiSlice';
 import { clearCartItems } from '../slices/cartSlice';
 
-const PlaceOrderPage = () => {
+const PlaceOrderScreen = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
-  const { shippingAddress, paymentMethod } = cart;
-
-  // Navigate to shipping page if shipping address is not set
-  useEffect(() => {
-    if (!shippingAddress) {
-      navigate('/shipping');
-    } else if (!paymentMethod) {
-      navigate('/payment');
-    }
-  }, [shippingAddress, paymentMethod, navigate]);
 
   const [createOrder, { isLoading, error }] = useCreateOrderMutation();
 
-  const placeOrderHandler = async (e) => {
-    try {
-      e.preventDefault();
+  useEffect(() => {
+    if (!cart.shippingAddress.address) {
+      navigate('/shipping');
+    } else if (!cart.paymentMethod) {
+      navigate('/payment');
+    }
+  }, [cart.paymentMethod, cart.shippingAddress.address, navigate]);
 
+  const dispatch = useDispatch();
+  const placeOrderHandler = async () => {
+    try {
       const res = await createOrder({
         orderItems: cart.cartItems,
         shippingAddress: cart.shippingAddress,
@@ -42,11 +36,10 @@ const PlaceOrderPage = () => {
         taxPrice: cart.taxPrice,
         totalPrice: cart.totalPrice,
       }).unwrap();
-
       dispatch(clearCartItems());
-      navigate(`/orders/${res._id}`);
-    } catch (error) {
-      toast.error(error?.data?.message);
+      navigate(`/order/${res._id}`);
+    } catch (err) {
+      toast.error(err);
     }
   };
 
@@ -59,35 +52,35 @@ const PlaceOrderPage = () => {
             <ListGroup.Item>
               <h2>Shipping</h2>
               <p>
-                <strong>Address: </strong>
-                {cart.shippingAddress.address}, {cart.shippingAddress.city}, {cart.shippingAddress.postalCode},{' '}
+                <strong>Address:</strong>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city} {cart.shippingAddress.postalCode},{' '}
                 {cart.shippingAddress.country}
               </p>
             </ListGroup.Item>
+
             <ListGroup.Item>
               <h2>Payment Method</h2>
-              <p>
-                <strong>Method: </strong>
-                {cart.paymentMethod}
-              </p>
+              <strong>Method: </strong>
+              {cart.paymentMethod}
             </ListGroup.Item>
+
             <ListGroup.Item>
               <h2>Order Items</h2>
-              {cart?.cartItems?.length === 0 ? (
-                <p>Your cart is empty</p>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty</Message>
               ) : (
                 <ListGroup variant="flush">
-                  {cart?.cartItems?.map((item, index) => (
+                  {cart.cartItems.map((item, index) => (
                     <ListGroup.Item key={index}>
                       <Row>
                         <Col md={1}>
                           <Image src={item.image} alt={item.name} fluid rounded />
                         </Col>
                         <Col>
-                          <Link to={`/product/${item._id}`}>{item.name}</Link>
+                          <Link to={`/product/${item.product}`}>{item.name}</Link>
                         </Col>
                         <Col md={4}>
-                          {item.quantity} x {item.price} = ${item.quantity * item.price}
+                          {item.quantity} x ${item.price} = ${(item.quantity * (item.price * 100)) / 100}
                         </Col>
                       </Row>
                     </ListGroup.Item>
@@ -129,12 +122,7 @@ const PlaceOrderPage = () => {
               </ListGroup.Item>
               <ListGroup.Item>{error && <Message variant="danger">{error.data.message}</Message>}</ListGroup.Item>
               <ListGroup.Item>
-                <Button
-                  type="button"
-                  className="btn-block"
-                  disabled={cart.cartItems.length === 0}
-                  onClick={placeOrderHandler}
-                >
+                <Button type="button" className="btn-block" disabled={cart.cartItems === 0} onClick={placeOrderHandler}>
                   Place Order
                 </Button>
                 {isLoading && <Loader />}
@@ -147,4 +135,4 @@ const PlaceOrderPage = () => {
   );
 };
 
-export default PlaceOrderPage;
+export default PlaceOrderScreen;
