aa5109691c5fa70852004e418c2c8c8e5eeb10b2
prasadhonrao <honrao.prasad@gmail.com>
2024-11-15 23:34:04 +0000

    Update review count reference from numReviews to totalReviews in Product and ProductPage components

--- src/webapp/src/components/Product.jsx
+++ src/webapp/src/components/Product.jsx
@@ -16,7 +16,7 @@ const Product = ({ product }) => {
           </Card.Title>
         </Link>
         <Card.Text as="div">
-          <Rating value={product.rating} text={`${product.numReviews} reviews`}></Rating>
+          <Rating value={product.rating} text={`${product.totalReviews} reviews`}></Rating>
         </Card.Text>
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
--- src/webapp/src/pages/ProductPage.jsx
+++ src/webapp/src/pages/ProductPage.jsx
@@ -70,7 +70,7 @@ const ProductPage = () => {
                   <h3>{product.name}</h3>
                 </ListGroup.Item>
                 <ListGroup.Item>
-                  <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                  <Rating value={product.rating} text={`${product.totalReviews} reviews`} />
                 </ListGroup.Item>
                 <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
                 <ListGroup.Item>Description: {product.description}</ListGroup.Item>
