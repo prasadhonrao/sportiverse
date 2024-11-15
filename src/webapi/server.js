8d2497d792fedf6d1c62df9d77520fb3b91e8be2
prasadhonrao <honrao.prasad@gmail.com>
2024-11-15 13:24:54 +0000

    Refactor server routes and static file handling for production and development environments

--- src/webapi/server.js
+++ src/webapi/server.js
@@ -31,7 +31,7 @@ app.use(cookieParser()); // Cookie parser is used to parse cookies
 connectDB();
 
 // Custom routes
-app.use('/', homeRoutes);
+app.use('/api/home', homeRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -40,9 +40,18 @@ app.use('/api/upload', uploadRoutes);
 // PayPal routes
 app.get('/api/config/paypal', (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID }));
 
-// Make uploads folder static
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '../webapp/build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'webapp', 'build', 'index.html')));
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 // Custom middlewares
 app.use(errorHandler);
