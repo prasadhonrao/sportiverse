d4685d0ad3c2f63eee593e6a2e8f12bb8d1d0fc9
prasadhonrao <honrao.prasad@gmail.com>
2024-11-15 21:41:42 +0000

    Add checkObjectId middleware to validate Mongoose ObjectId and update product routes

--- src/webapi/middlewares/errorHandler.js
+++ src/webapi/middlewares/errorHandler.js
@@ -2,13 +2,6 @@ function errorHandler(err, req, res, next) {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
-  // Mongoose bad ObjectId
-  if (err.name === 'CastError' && err.kind === 'ObjectId') {
-    console.log('Invalid ObjectId');
-    message = 'Invalid MongoDB ObjectId';
-    statusCode = 404;
-  }
-
   res.status(statusCode).json({
     message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
--- src/webapi/routes/productRoutes.js
+++ src/webapi/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { protect, admin } from '../middlewares/authMiddleware.js';
+import checkObjectId from '../middlewares/checkObjectId.js';
 import {
   getProducts,
   getProductById,
@@ -12,8 +13,12 @@ import {
 
 const router = express.Router();
 router.route('/').get(getProducts).post(protect, admin, createProduct);
-router.route('/:id/reviews').post(protect, createProductReview);
+router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
 router.get('/top', getTopProducts);
-router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router
+  .route('/:id')
+  .get(checkObjectId, getProductById)
+  .put(protect, admin, checkObjectId, updateProduct)
+  .delete(protect, admin, checkObjectId, deleteProduct);
 
 export default router;
