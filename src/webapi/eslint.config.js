2392b51f580fff2a41f1821126aad7db7460a1d0
prasadhonrao <honrao.prasad@gmail.com>
2024-11-16 22:54:59 +0000

    Add ESLint configuration and lint script to package.json

---
+++ src/webapi/eslint.config.js
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    ignores: ['node_modules/', 'build/'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest, // Add Jest globals
+        fetch: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'error',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: 'colors', argsIgnorePattern: '^next$' }],
+    },
+  },
+  pluginJs.configs.recommended,
+];
