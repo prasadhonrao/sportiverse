88ed9fcc489d99ac3f1f57ccafb8f8335be55a5f
prasadhonrao <honrao.prasad@gmail.com>
2024-11-22 08:30:04 +0000

    Update Dockerfile and dockerize script to use REACT_APP_BASE_API_URI for build-time configuration

--- src/webapp/dockerize.sh
+++ src/webapp/dockerize.sh
@@ -1,2 +1,7 @@
-docker build -t prasadhonrao/sportiverse-webapp .
-docker push prasadhonrao/sportiverse-webapp
+# Dev image build
+docker build --build-arg REACT_APP_BASE_API_URI=http://localhost:5000 -t prasadhonrao/sportiverse-webapp:dev .
+docker push prasadhonrao/sportiverse-webapp:dev
+
+# Prod image build
+# docker build --build-arg REACT_APP_BASE_API_URI=http://sportiverse.webapi -t prasadhonrao/sportiverse-webapp:prod .
+# docker push prasadhonrao/sportiverse-webapp:prod
