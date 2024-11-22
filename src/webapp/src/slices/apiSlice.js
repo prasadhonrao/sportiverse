6394de1315e98c425d37349129c64f4b5b533f67
prasadhonrao <honrao.prasad@gmail.com>
2024-11-22 08:10:10 +0000

    Add example environment file and update API base URL handling

--- src/webapp/src/slices/apiSlice.js
+++ src/webapp/src/slices/apiSlice.js
@@ -1,6 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../constants';
 
+console.log(`Env: ${process.env.NODE_ENV}`);
+console.log(`Base URL: ${BASE_URL}`);
+
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   tagTypes: ['Product', 'User', 'Order'],
