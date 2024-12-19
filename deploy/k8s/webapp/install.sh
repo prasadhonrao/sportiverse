8dc86f61bba2bee21dc47da4c3be7c361769db78
prasadhonrao <honrao.prasad@gmail.com>
2024-12-19 16:36:20 +0000

    Add liveness and readiness probes, service account, and LoadBalancer service for webapp; update install/uninstall scripts

--- deploy/k8s/webapp/install.sh
+++ deploy/k8s/webapp/install.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 kubectl apply -f configmap.yaml
+kubectl apply -f serviceaccount.yaml
 kubectl apply -f role.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
--- deploy/k8s/webapp/uninstall.sh
+++ deploy/k8s/webapp/uninstall.sh
@@ -1,6 +1,7 @@
 #!bin/bash
-kubectl delete -f configmap.yaml
-kubectl delete -f deployment.yaml
-kubectl delete -f service.yaml
 kubectl delete -f hpa.yaml
+kubectl delete -f service.yaml
+kubectl delete -f deployment.yaml
 kubectl delete -f role.yaml
+kubectl delete -f serviceaccount.yaml
+kubectl delete -f configmap.yaml
