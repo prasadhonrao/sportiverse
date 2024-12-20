3c58a776241c9b70ae2eb463d0f30912404ceec0
prasadhonrao <honrao.prasad@gmail.com>
2024-12-20 10:15:49 +0000

    Remove deprecated Kubernetes database deployment and related resources

--- deploy/k8s/database/install.sh
+++ deploy/k8s/database/install.sh
@@ -3,19 +3,7 @@ kubectl apply -f configmap.yaml
 kubectl apply -f secret.yaml
 kubectl apply -f role.yaml
 kubectl apply -f serviceaccount.yaml
-# kubectl apply -f pv.yaml
-# kubectl apply -f pvc.yaml
-kubectl apply -f storageclass.yaml
+# kubectl delete -f storageclass.yaml
+kubectl apply -f storageclass.yaml # Storage class fields are immutable, so we need to delete and reapply
 kubectl apply -f statefulset.yaml
-
-# Wait for PVC to be bound
-# echo "Waiting for PVC to be bound..."
-# while [[ $(kubectl get pvc sportiverse-pvc -n sportiverse-namespace -o jsonpath='{.status.phase}') != "Bound" ]]; do
-#   echo -n "."
-#   sleep 1
-# done
-# echo "PVC is bound."
-
-# kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
-kubectl apply -f hpa.yaml
--- deploy/k8s/database/uninstall.sh
+++ deploy/k8s/database/uninstall.sh
@@ -1,12 +1,8 @@
 #!bin/bash
-kubectl delete -f configmap.yaml
-kubectl delete -f secret.yaml
-kubectl delete -f deployment.yaml
-kubectl delete -f statefulset.yaml
 kubectl delete -f service.yaml
-kubectl delete -f serviceaccount.yaml
+kubectl delete -f statefulset.yaml
 kubectl delete -f storageclass.yaml
-kubectl delete -f pvc.yaml
-kubectl delete -f pv.yaml
-kubectl delete -f hpa.yaml
+kubectl delete -f serviceaccount.yaml
 kubectl delete -f role.yaml
+kubectl delete -f secret.yaml
+kubectl delete -f configmap.yaml
