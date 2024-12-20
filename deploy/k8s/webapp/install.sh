ff2b4476799cea2b54f4639c680d50a63e531168
prasadhonrao <honrao.prasad@gmail.com>
2024-12-20 14:42:24 +0000

    Enhance Kubernetes ingress configuration for CORS support and adjust resource limits for webapp deployment

--- deploy/k8s/webapp/install.sh
+++ deploy/k8s/webapp/install.sh
@@ -4,4 +4,4 @@ kubectl apply -f serviceaccount.yaml
 kubectl apply -f role.yaml
 kubectl apply -f deployment.yaml
 kubectl apply -f service.yaml
-kubectl apply -f hpa.yaml
+# kubectl apply -f hpa.yaml
--- deploy/k8s/webapp/uninstall.sh
+++ deploy/k8s/webapp/uninstall.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-kubectl delete -f hpa.yaml
+# kubectl delete -f hpa.yaml
 kubectl delete -f service.yaml
 kubectl delete -f deployment.yaml
 kubectl delete -f role.yaml
